0512a7b6130cb9d8e449083e9c85acc7b6f509fd
Grzegorz Holak <holi87@gmail.com>
2019-07-20 18:35:24 +0200

    wprowadzanie danych

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -1,28 +1,23 @@
 package library.app;
 
+import library.io.DataReader;
 import library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka";
-        final String appVersion = "0.7";
+        final String appVersion = "0.8";
         Book[] books = new Book[1000];
+        DataReader dataReader = new DataReader();
 
-        books[0] = new Book("Diuna", "Frank Herbert", 2018, 670,
-                "Rebis", "9788373017238");
-        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
-                2009, 352, "Helion", "9788324620845");
-        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
-                "Bert Bates, Katherine Sierra", 2008, 851,
-                "McGraw-Hill Osborne Media", "9780071591065");
-        books[3] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
-                2010, 296, "Greg", "9788373271890");
         System.out.println(appName+" "+appVersion);
+        System.out.println("Wprowadź nową książkę");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
         System.out.println("Książki dostępne w bibliotece");
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
-        books[3].printInfo();
         System.out.println("Można przechowywac maksymalnie " + books.length + " książek");
 
 
