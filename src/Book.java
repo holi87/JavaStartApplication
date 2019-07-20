24960cae33971cd8f0df0d901c4a27fdfe4140a7
Grzegorz Holak <holi87@gmail.com>
2019-07-20 15:18:10 +0200

    konstruktory

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,25 @@
 class Book {
-
     String title;
     String author;
     int releaseDate;
     int pages;
     String publisher;
     String isbn;
+
+    Book(String bookTitle, String bookAuthor, int bookReleasedate, int bookPages, String bookPublisher, String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookReleasedate;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+    String getInfo() {
+        return title + ";" + author + ";" + releaseDate + ";" + pages + ";" + publisher + ";" + isbn;
+    }
+
+    void printInfo() {
+        System.out.println(getInfo());
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -3,23 +3,22 @@ public class Library {
         final String appName = "Biblioteka";
         final String appVersion = "0.1";
 
-        Book book1 = new Book();
-
-
-        book1.title = "Diuna";
-        book1.author = "Frank Herbert";
-        book1.releaseDate = 2018;
-        book1.pages = 670;
-        book1.publisher = "Rebis";
-        book1.isbn = "9788373017238";
-
+        Book book1 = new Book("Diuna", "Frank Herbert", 2018, 670,
+                "Rebis", "9788373017238");
+        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
+                2009, 352, "Helion", "9788324620845");
+        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+                "Bert Bates, Katherine Sierra", 2008, 851,
+                "McGraw-Hill Osborne Media", "9780071591065");
+        Book book4 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
+                2010, 296, "Greg", "9788373271890");
         System.out.println(appName+" "+appVersion);
         System.out.println("Książki dostępne w bibliotece");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
+        book4.printInfo();
+
+
     }
 }
