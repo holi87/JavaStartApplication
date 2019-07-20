0fe34807aedc95052a8d6a112ab7e815118926d9
Grzegorz Holak <holi87@gmail.com>
2019-07-20 13:37:38 +0200

    klasy i obiekty zadanie

--- src/Library.java
+++ src/Library.java
@@ -3,20 +3,23 @@ public class Library {
         final String appName = "Biblioteka";
         final String appVersion = "0.1";
 
-        String title = "Diuna";
-        String author = "Frank Herbert";
-        int releaseDate = 2018;
-        int pages = 670;
-        String publisher = "Rebis";
-        String isbn = "9788373017238";
+        Book book1 = new Book();
+
+
+        book1.title = "Diuna";
+        book1.author = "Frank Herbert";
+        book1.releaseDate = 2018;
+        book1.pages = 670;
+        book1.publisher = "Rebis";
+        book1.isbn = "9788373017238";
 
         System.out.println(appName+" "+appVersion);
         System.out.println("Książki dostępne w bibliotece");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
     }
 }
